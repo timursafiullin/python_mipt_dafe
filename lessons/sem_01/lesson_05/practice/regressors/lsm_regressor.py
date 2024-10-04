3327e950f01e90f761d84c8adf53d61447a4305f
TimurSafiullin <safiullintb@yandex.ru>
2024-10-04 19:07:07 +0300

    Did Home Assignments - Regression

--- lessons/sem_01/lesson_05/practice/regressors/lsm_regressor.py
+++ lessons/sem_01/lesson_05/practice/regressors/lsm_regressor.py
@@ -1,14 +1,51 @@
 from typing import Sequence, Union
 from numbers import Real
+from numpy import average
 
 from regressors.regressor_abc import RegressorABC
 
 
 class RegressorLSM(RegressorABC):
+
+    _abscissa: list[Real]
+    _ordinates: list[Real]
+    _b: Real
+    _a: Real
+
+    def __init__(self) -> None:
+
+        self._abscissa = []
+        self._ordinates = []
+        self._b = 0.0
+        self._a = 0.0
+
     def fit(self, abscissa: Sequence[Real], ordinates: Sequence[Real]) -> None:
-        # ваш код
-        pass
+
+        if len(abscissa) != len(ordinates):
+            raise ValueError(f"Shape mismatch: {len(abscissa)} != {len(ordinates)}")
+
+        self._abscissa = list(abscissa)
+        self._ordinates = list(ordinates)
+        
+        x_avg = average(self._abscissa)
+        y_avg = average(self._ordinates)
+        x_squares_avg = average([x**2 for x in self._abscissa])
+        xy_avg = average([x*y for x, y in zip(self._abscissa, self._ordinates)])
+
+        self._a = (xy_avg - (x_avg * y_avg)) / (x_squares_avg - (x_avg ** 2))
+        self._b = y_avg - self._a * x_avg
+        
 
     def predict(self, abscissa: Union[Real, Sequence[Real]]) -> list:
-        # ваш код
-        return [abscissa] if isinstance(abscissa, Real) else abscissa
+        if self._abscissa is None or self._ordinates is None:
+            raise RuntimeError("Call method fit() firstly")
+
+        if isinstance(abscissa, Real):
+            return [self._get_approximation(abscissa)]
+
+        return [self._get_approximation(abscissa_i) for abscissa_i in abscissa]
+    
+
+    def _get_approximation(self, abscissa: Real) -> float:
+        return self._a * abscissa + self._b
+
--- lessons/sem_01/lesson_05/practice/regressors/nonparametric_regressor.py
+++ lessons/sem_01/lesson_05/practice/regressors/nonparametric_regressor.py
@@ -5,10 +5,76 @@ from regressors.regressor_abc import RegressorABC
 
 
 class NonparametricRegressor(RegressorABC):
+
+    NEAREST_LIMIT: int = 1
+
+    _abscissa: list[Real]
+    _ordinates: list[Real]
+    _h_nearest: int
+
+    def __init__(self, h_nearest: int = 5) -> None:
+
+        self._h_nearest = round(h_nearest)
+
+        # Validate h_nearest input
+        if self._h_nearest < self.NEAREST_LIMIT:
+            raise ValueError(f"ValueError: {h_nearest} is lower than limit NEAREST_LIMIT = {self.NEAREST_LIMIT}")
+
+        self._abscissa = []
+        self._ordinates = []
+
     def fit(self, abscissa: Sequence[Real], ordinates: Sequence[Real]) -> None:
-        # ваш код
-        pass
+        
+        if len(abscissa) != len(ordinates):
+            raise ValueError(f"Shape mismatch: {len(abscissa)} != {len(ordinates)}")
+
+        self._abscissa = list(abscissa)
+        self._ordinates = list(ordinates)
+
+
 
     def predict(self, abscissa: Union[Real, Sequence[Real]]) -> list:
-        # ваш код
-        return abscissa
+        if self._abscissa is None or self._ordinates is None:
+            raise RuntimeError("fit must be called first")
+
+        if isinstance(abscissa, Real):
+            return [self._get_approximation(abscissa)]
+
+        return [
+            self._get_approximation(abscissa_i) for abscissa_i in abscissa
+        ]
+    
+    def _get_approximation(self, abscissa: Real) -> float:
+        h_index = min(self._h_nearest, len(self._abscissa) - 1)
+        h_nearest=  sorted(self._abscissa, key=lambda x: abs(x - abscissa))[h_index]
+        window_size = abs(abscissa - h_nearest)
+
+        weights = self._get_weights(abscissa, window_size)
+
+        numerator = sum(
+            map(lambda tup_: tup_[0] * tup_[1], zip(self._ordinates, weights)),
+        )
+        return numerator / sum(weights)
+
+    def _get_weights(self, abscissa: Real, window_size: Real) -> list[float]:
+
+        weights = []
+
+        # Ability to change Kernel
+        def Kernel(z: Real):
+            if abs(z) < 1:
+                return 0.75*(1 - z**2)
+            else:
+                return 0
+            
+        for abscissa_i in self._abscissa:
+            kernel_value = abs(abscissa_i - abscissa) / window_size
+            weight = Kernel(kernel_value)
+            weights.append(weight)
+        
+        return weights
+
+
+        
+
+
