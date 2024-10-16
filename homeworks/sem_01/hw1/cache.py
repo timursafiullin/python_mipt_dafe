2496d505eaeaf72a9038c3350d1793df87c8df5f
TimurSafiullin <safiullintb@yandex.ru>
2024-10-17 00:04:29 +0300

    Realised LRU_CACHE

--- homeworks/sem_01/hw1/cache.py
+++ homeworks/sem_01/hw1/cache.py
@@ -3,7 +3,6 @@ from typing import (
     TypeVar,
 )
 
-
 T = TypeVar("T")
 
 
@@ -11,7 +10,7 @@ def lru_cache(capacity: int) -> Callable[[T], T]:
     """
     Параметризованный декоратор для реализации LRU-кеширования.
 
-    Args:
+    arguments:
         capacity: целое число, максимальный возможный размер кеша.
 
     Returns:
@@ -22,5 +21,36 @@ def lru_cache(capacity: int) -> Callable[[T], T]:
             для получения целого числа.
         ValueError, если после округления capacity - число, меньшее 1.
     """
-    # ваш код
-    pass
+    try:
+        capacity = int(round(capacity))
+        if capacity < 1:
+            raise ValueError("Capacity should be equal or more than 1.")
+    except:
+        raise TypeError("Capacity should be a number.")
+
+    cache = {}
+
+    def _lru_realise(func):
+        def wrapper(*args, **kwargs):
+            arguments = (args, frozenset(sorted(kwargs.items())))
+
+            if arguments not in tuple(cache.keys()):
+                result = func(*args, **kwargs)
+
+                if len(cache) > capacity:
+                    # Удаление первого элемента
+                    for k in cache.keys():
+                        del cache[k]
+                        break
+                cache[arguments] = result
+                return cache[arguments]
+            
+            # Перевод в "начало"
+            copy = cache[arguments]
+            del cache[arguments]
+            cache[arguments] = copy
+
+            return cache[arguments]
+        return wrapper
+    return _lru_realise
+
