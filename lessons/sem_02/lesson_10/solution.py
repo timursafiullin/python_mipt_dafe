041b4ae08358c946ad2dbd409efe24a0ee46e942
timursafiullin <safiullintb@yandex.ru>
2025-04-11 20:11:39 +0300

    Solved contest lesson10

--- lessons/sem_02/lesson_10/solution.py
+++ lessons/sem_02/lesson_10/solution.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import seaborn as sns
-import numpy as np
+import warnings
 
 
+warnings.filterwarnings("ignore")
 titanic_data = sns.load_dataset("titanic")
 
 
-def count_nulls(data): #1
+def count_nulls(data):
     return data.isnull().sum()
 
 
-def delete_nulls(data): #2
+def delete_nulls(data):
     rows_half = data.shape[0] / 2
     cols_half = data.shape[1] / 2
     for column in data.columns:
@@ -21,7 +22,7 @@ def delete_nulls(data): #2
             data.drop(index, inplace=True)
 
 
-def fill_nulls(data): #3
+def fill_nulls(data):
     men_mask = data['who'] == 'man'
     women_mask = data['who'] == 'woman'
     child_mask = data['who'] == 'child'
@@ -35,17 +36,17 @@ def fill_nulls(data): #3
     data.loc[child_mask, 'age'] = data.loc[child_mask, 'age'].fillna(child_age)
 
 
-def delete_withnull_rows(data): #4
+def delete_withnull_rows(data):
     for index, row in data.iterrows():
         if row.isnull().sum() > 1:
             data.drop(index, inplace=True)
 
 
-def from_city_max(data): #5
+def from_city_max(data):
     return data['embark_town'].value_counts().idxmax()
 
 
-def survivors_proportion(data, tag=None): #6, #8
+def survivors_proportion(data, tag=None):
     match tag:
         case None:
             return (data['survived'].value_counts(normalize=True)[1]) * 100
@@ -58,41 +59,59 @@ def survivors_proportion(data, tag=None): #6, #8
             return pd.Series(result, index=unique)
 
 
-def survivors_by_city(data): #7
+def survivors_by_city(data):
     return data[data['survived'] == 1]['embark_town'].value_counts()
 
 
-# # Task 1
-# print("\033[1mTask 1:\033[0m\n")
-# print( count_nulls(titanic_data) )
-# 
-# # Task 2
-# print("\n\033[1mTask 2:\033[0m\n")
-# delete_nulls(titanic_data)
-# print( titanic_data.isnull().sum() )
-# 
-# # Task 3
-# print("\n\033[1mTask 3:\033[0m\n")
-# fill_nulls(titanic_data)
-# print( titanic_data.isnull().sum() )
-# 
-# # Task 4
-# print("\n\033[1mTask 4:\033[0m\n")
-# delete_withnull_rows(titanic_data)
-# print( titanic_data.isnull().sum() )
-# 
-# # Task 5
-# print("\n\033[1mTask 5:\033[0m\n")
-# print("Больше всего пассажиров прибыли из города:", from_city_max(titanic_data) )
-# 
-# # Task 6
-# print("\n\033[1mTask 6:\033[0m\n")
-# print("Доля выживших пассажиров: {:.2f}%".format(survivors_proportion(titanic_data)))
+def rich_survivors(data):
+    rich = data[data['fare'] >= 100]['survived'].value_counts(normalize=True)[1]
+    return rich * 100
 
-# # Task 7
-# print("\n\033[1mTask 7:\033[0m\n")
-# print("Количество выживших пассажиров в городах:\n", survivors_by_city(titanic_data))
 
-# # Task 8
-# print("\n\033[1mTask 8:\033[0m\n")
-# print("Доля выживших пассажиров по классам:\n", survivors_proportion(titanic_data, 'class'))
+def alone_child(data):
+    alone = data[data["who"] == "child"][data["alone"] == True]
+    return alone.shape[0]
+
+
+# Task 1
+print("\033[1mTask 1:\033[0m\n")
+print(count_nulls(titanic_data))
+
+# Task 2
+print("\n\033[1mTask 2:\033[0m\n")
+delete_nulls(titanic_data)
+print(titanic_data.isnull().sum())
+
+# Task 3
+print("\n\033[1mTask 3:\033[0m\n")
+fill_nulls(titanic_data)
+print(titanic_data.isnull().sum())
+
+# Task 4
+print("\n\033[1mTask 4:\033[0m\n")
+delete_withnull_rows(titanic_data)
+print(titanic_data.isnull().sum())
+
+# Task 5
+print("\n\033[1mTask 5:\033[0m\n")
+print("Больше всего пассажиров прибыли из города:", from_city_max(titanic_data))
+
+# Task 6
+print("\n\033[1mTask 6:\033[0m\n")
+print("Доля выживших пассажиров: {:.2f}%".format(survivors_proportion(titanic_data)))
+
+# Task 7
+print("\n\033[1mTask 7:\033[0m\n")
+print("Количество выживших пассажиров по городам:\n", survivors_by_city(titanic_data))
+
+# Task 8
+print("\n\033[1mTask 8:\033[0m\n")
+print("Доля выживших пассажиров по классам:\n", survivors_proportion(titanic_data, 'class'))
+
+# Task 9
+print("\n\033[1mTask 9:\033[0m\n")
+print("Доля выживших среди богатых пассажиров: {:.2f}%".format(rich_survivors(titanic_data)))
+
+# Task 10
+print("\n\033[1mTask 10:\033[0m\n")
+print("Количество детей путешествоваших в одиночку: ", alone_child(titanic_data))
